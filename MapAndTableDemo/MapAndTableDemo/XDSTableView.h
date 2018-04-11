3ac2bf67912ebdac9b2bad2436606c3f53526a8a
XuDongShun <1533889695@qq.com>
2018-04-11 20:49:00 +0800

    Revert "Revert "滴滴出行首页/高德地图/上拉底部TableView/地图+tableView""
    This reverts commit 48888c93fc60983aecb500550a7a92b88bcabcb8.

---
+++ MapAndTableDemo/MapAndTableDemo/XDSTableView.h
@@ -0,0 +1,15 @@
+//
+//  XDSTableView.h
+//  MapTableViewDemo
+//
+//  Created by cdj on 2018/4/11.
+//  Copyright © 2018年 Felix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XDSTableView : UITableView
+
+
+
+@end
